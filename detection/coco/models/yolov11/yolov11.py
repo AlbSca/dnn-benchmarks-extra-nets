08775095c3ad3c89ed0a9462c4bc9b5f5593a94a
albsca <10656563@polimi.it>
2025-11-20 14:21:42 +0100

    disable layer fusing for yolo

--- detection/coco/models/yolov11/yolov11.py
+++ detection/coco/models/yolov11/yolov11.py
@@ -3,6 +3,8 @@ from ultralytics import YOLO
 
 def get_yolov11(weights_dir):
     weights_path = os.path.join(weights_dir, 'yolo11n.pt') #TODO update weights
-    model = YOLO(weights_path)
+    model = YOLO(weights_path, verbose=False)
     model.eval()
+    # disable layer fusing. This is necessary, otherwise the individual layers to inject won't be available during simulation
+    model.model.is_fused = lambda: True
     return model
